f9a294cb09225524eb9d2262fa4cad38f630bb49
Seth Kitchen <sethkitchen@Seths-MacBook-Air.local>
2020-12-23 17:20:52 -0600

    last appveyor -- didnt save the file..duh

--- buildLibrariesNuget.cmd
+++ buildLibrariesNuget.cmd
@@ -1,5 +1,3 @@
 SET PACKAGE_VERSION="1.0.0"
 
-mkdir .\src
-mkdir .\src\nuget
-dotnet pack SoilMechanicsLibrary/SoilMechanicsLibrary.csproj -c release --no-build --output ../nuget /p:PackageVersion=%PACKAGE_VERSION%
+dotnet pack SoilMechanicsLibrary/SoilMechanicsLibrary.csproj -c release --no-build --output ./nuget /p:PackageVersion=%PACKAGE_VERSION%

91a31a7585feee16ad071a20a05c4d6c086305d1
Seth Kitchen <sethkitchen@Seths-MacBook-Air.local>
2020-12-30 21:23:24 -0600

    add test utils for full coverage

---
+++ SoilMechanicsLibraryTests/Shared/TestUtils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+using SoilMechanicsLibrary.Shared;
+
+namespace SoilMechanicsLibraryTests.Shared
+{
+    public class TestUtils
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void DescriptionShouldReturnEmptyStringOnNonEnum()
+        {
+            string d = Utils.GetDescription<string>(null);
+            Assert.IsEmpty(d);
+        }
+    }
+}
